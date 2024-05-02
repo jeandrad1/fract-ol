ace1b6ae54870a0d816fdfd589004521c4fd2dc1
jeandrad1 <jesusandradeperez@gmail.com>
2024-05-02 14:35:32 +0200

    added struct

--- fractol.h
+++ fractol.h
@@ -27,3 +27,23 @@ typedef struct image
     int		line_length;
     int		endin;
 }	t_image;
+
+typedef struct		s_fractol
+{
+	void			*mlx;
+	void			*window;
+	t_image			*image;
+	int				max_iteration;
+	t_complex		min;
+	t_complex		max;
+	t_complex		factor;
+	t_complex		c;
+	t_complex		k;
+	t_bool			is_julia_fixed;
+	int				start_line;
+	int				finish_line;
+	int				color_shift;
+	int				(*formula)(struct s_fractol *fractol);
+	t_bool			is_help;
+}					t_fractol;
+
