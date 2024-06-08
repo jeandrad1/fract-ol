4b95762b294e1e197471bcfc24064eb5ed6122e5
jeandrad1 <jesusandradeperez@gmail.com>
2024-06-08 09:46:48 +0200

    Added change zoom

---
+++ change_zoom.c
@@ -0,0 +1,49 @@
+#include "../../includes/fractol.h"
+
+void	zoom(t_fractol *fractol, int x, int y, double zoom_factor)
+{
+	double	x_math;
+	double	y_math;
+	double	new_x_range;
+	double	new_y_range;
+
+	x_math = fractol->min_r + ((double)x / WIDTH)
+		* (fractol->max_r - fractol->min_r);
+	y_math = fractol->min_i + (1 - (double)y / HEIGHT)
+		* (fractol->max_i - fractol->min_i);
+	new_x_range = (fractol->max_r - fractol->min_r) / zoom_factor;
+	new_y_range = (fractol->max_i - fractol->min_i) / zoom_factor;
+	fractol->min_r = x_math - ((double)x / WIDTH) * new_x_range;
+	fractol->max_r = fractol->min_r + new_x_range;
+	fractol->min_i = y_math - (1 - (double)y / HEIGHT) * new_y_range;
+	fractol->max_i = fractol->min_i + new_y_range;
+}
+
+void	move(t_fractol *f, double distance, char direction)
+{
+	double	center_r;
+	double	center_i;
+
+	center_r = f->max_r - f->min_r;
+	center_i = f->max_i - f->min_i;
+	if (direction == 'R')
+	{
+		f->min_r += center_r * distance;
+		f->max_r += center_r * distance;
+	}
+	else if (direction == 'L')
+	{
+		f->min_r -= center_r * distance;
+		f->max_r -= center_r * distance;
+	}
+	else if (direction == 'U')
+	{
+		f->min_i += center_i * distance;
+		f->max_i += center_i * distance;
+	}
+	else if (direction == 'D')
+	{
+		f->min_i -= center_i * distance;
+		f->max_i -= center_i * distance;
+	}
+}
