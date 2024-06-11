4805598704a394e3b0f4e8ab7dacd9dd4751b2f0
jeandrad1 <jesusandradeperez@gmail.com>
2024-06-11 19:49:49 +0200

    changes in the information displayed

--- src/info.c
+++ src/info.c
@@ -6,7 +6,7 @@
 /*   By: jeandrad <jeandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 11:10:24 by jeandrad          #+#    #+#             */
-/*   Updated: 2024/06/08 19:07:51 by jeandrad         ###   ########.fr       */
+/*   Updated: 2024/06/11 19:43:04 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 
 void	help_msg(void)
 {
-	ft_printf("⊱ ───────────────── {.⋅ Fractol ⋅.} ───────────────── ⊰\n\n");
-	ft_printf("Which fractal would you like to see?\n");
-	ft_printf("J - Julia    ");
+	ft_printf("\nFractol\n\n");
+	ft_printf("Fractals avaliable:\n");
+	ft_printf("J - Julia");
 	ft_printf("\n\n");
-	ft_printf("\e[36mUsage example:\t./fractol <type>\n\t\t./fractol M\e[0m\n\n"\
-	);
+	ft_printf("Usage example:\t./fractol <type>\n\t\t./fractol M\n\n");
 	ft_printf("\nFor Julia, you may specify starting values which should be betw\
 	een -2.0 and 2.0.\n");
-	ft_printf("\e[36mUsage example:\t");
+	ft_printf("Interesting examples:\t");
 	ft_printf("./fractol J\n\t\t./fractol J 0.285 0.01\n");
 	ft_printf("\t\t./fractol J -0.4 0.6\n");
 	ft_printf("\t\t./fractol J -1.476 0.0\n");
-	ft_printf("\t\t./fractol J 0.3 -0.01\e[0m\n\n");
+	ft_printf("\t\t./fractol J 0.3 -0.01\n\n");
 }
