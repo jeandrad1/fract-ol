c6f7bd836c38c81ab1942069f733f8a672c1541e
jeandrad1 <152317568+jeandrad1@users.noreply.github.com>
2024-06-17 18:18:10 +0200

    Update dragon_fract_bonus.c

--- bonus/dragon_fract_bonus.c
+++ bonus/dragon_fract_bonus.c
@@ -11,23 +11,42 @@
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
-
-int	dragon(t_fractol *f)
+int	burning_ship_set(t_fractol *f)
 {
-	int		n;
-	double	temp;
+    int		n;
+    double	temp;
 
-	n = 0;
-	f->m_zi = 0;
-	f->m_zr = 0;
-	while (n < MAX_ITERATIONS)
-	{
-		if ((f->m_zr * f->m_zr + f->m_zi * f->m_zi) > 4)
-			break ;
-		temp = 2 * f->m_zr * f->m_zi + f->pi;
-		f->m_zr = f->m_zr * f->m_zr - f->m_zi * f->m_zi + f->pr;
-		f->m_zi = temp;
-		n++;
-	}
-	return (n);
+    n = 0;
+    f->m_zi = 0;
+    f->m_zr = 0;
+    while (n < MAX_ITERATIONS)
+    {
+        if ((f->m_zr * f->m_zr + f->m_zi * f->m_zi) > 4)
+            break ;
+        temp = 2 * fabs(f->m_zr) * fabs(f->m_zi) + f->pi;
+        f->m_zr = f->m_zr * f->m_zr - f->m_zi * f->m_zi + f->pr;
+        f->m_zi = temp;
+        n++;
+    }
+    return (n);
+}
+
+int	tricorn_set(t_fractol *f)
+{
+    int		n;
+    double	temp;
+
+    n = 0;
+    f->m_zi = 0;
+    f->m_zr = 0;
+    while (n < MAX_ITERATIONS)
+    {
+        if ((f->m_zr * f->m_zr + f->m_zi * f->m_zi) > 4)
+            break ;
+        temp = 2 * f->m_zr * -f->m_zi + f->pi; // use the complex conjugate (-f->m_zi instead of f->m_zi)
+        f->m_zr = f->m_zr * f->m_zr - f->m_zi * f->m_zi + f->pr;
+        f->m_zi = temp;
+        n++;
+    }
+    return (n);
 }
