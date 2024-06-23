cc1d3d0eafb622b94077407c7070371e672f6f7a
jeandrad1 <jesusandradeperez@gmail.com>
2024-06-23 18:29:39 +0200

    Fixed some Mandelbrot multiplications for powers

--- src/mandelbrot_fract.c
+++ src/mandelbrot_fract.c
@@ -6,7 +6,7 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 12:54:06 by jeandrad          #+#    #+#             */
-/*   Updated: 2024/06/22 11:18:03 by jeandrad         ###   ########.fr       */
+/*   Updated: 2024/06/23 18:28:34 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ int	mandelbrot_set(t_fractol *f)
 	f->m_zr = 0;
 	while (n < MAX_ITERATIONS)
 	{
-		if ((f->m_zr * f->m_zr + f->m_zi * f->m_zi) > 4)
+		if ((pow(f->m_zr, 2) + pow(f->m_zi, 2)) > 4)
 			break ;
 		temp = 2 * f->m_zr * f->m_zi + f->pi;
-		f->m_zr = f->m_zr * f->m_zr - f->m_zi * f->m_zi + f->pr;
+		f->m_zr = pow(f->m_zr, 2) - pow(f->m_zi, 2) + f->pr;
 		f->m_zi = temp;
 		n++;
 	}
