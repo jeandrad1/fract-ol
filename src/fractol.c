4db48aa127dbcbdd4253eb3c11cc6f46e9086e93
jeandrad1 <jesusandradeperez@gmail.com>
2024-06-23 18:07:38 +0200

    Removed a comment

--- src/fractol.c
+++ src/fractol.c
@@ -6,7 +6,7 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 11:11:00 by jeandrad          #+#    #+#             */
-/*   Updated: 2024/06/23 17:48:55 by jeandrad         ###   ########.fr       */
+/*   Updated: 2024/06/23 18:07:27 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,5 @@ int	main(int argc, char **argv)
 	mlx_key_hook(img.mlx, &handle_keyboard, &fractol);
 	mlx_loop(fractol.img.mlx);
 	mlx_terminate(fractol.img.mlx);
-	//	free_fractol(&fractol);
 	return (0);
 }
